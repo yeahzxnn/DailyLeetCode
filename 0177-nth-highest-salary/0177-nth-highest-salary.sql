2c8a7a1caa24411d114cfa52f73e58b40f3abf98
YEJIN SHIN (KAIA) <yejin0621@tukorea.ac.kr>
2026-02-18 14:36:25 +0900

    Time: 431 ms (53.55%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    SELECT DISTINCT salary
+    FROM(
+        SELECT salary,
+        DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+        FROM Employee
+    ) t
+    WHERE rnk = N
+  );
+END
